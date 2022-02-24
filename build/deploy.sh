549df50fa440e981e83ff499adaa098480f52976
Dreamn <37787014+dreamncn@users.noreply.github.com>
2022-02-24 22:18:43 +0800

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,10 +2,10 @@
 # 确保脚本抛出遇到的错误
 set -e
 npm run build # 生成静态文件
-#cd docs/.vuepress/dist # 进入生成的文件夹
+cd docs/.vuepress/dist # 进入生成的文件夹
 # deploy to github
 echo 'auto.ankio.net' > CNAME
-mv docs/.vuepress/dist ../docs
+#mv docs/.vuepress/dist ../docs
 if [ -z "$ANKIO" ]; then
   msg='deploy'
   githubUrl=git@github.com:dreamncn/AutoResource.git
