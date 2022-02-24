1836adf478585d62c903e9937335690ea86c8b6a
Dreamn <37787014+dreamncn@users.noreply.github.com>
2022-02-24 18:07:34 +0800

    Create deploy.sh

---
+++ docs/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+# 确保脚本抛出遇到的错误
+set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
+
+# deploy to github
+echo 'auto.ankio.net' > CNAME
+if [ -z "$ANKIO" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:dreamncn/AutoResource.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://dreamncn:${GITHUB_TOKEN}@github.com/dreamncn/AutoResource.git
+  git config --global user.name "ankio"
+  git config --global user.email "dream@dreamn.cn"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
+
+cd -
+rm -rf docs/.vuepress/dist
