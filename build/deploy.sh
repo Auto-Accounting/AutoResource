96e6f78de1dff9158e67c2ea8ef397d9b6279aea
ankio <dream@dreamn.cn>
2022-03-03 16:49:18 +0800

    commit action

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
-yarn build # 生成静态文件
-cd docs/.vuepress/dist # 进入生成的文件夹
+ls ./build/auto-tools/dist
+cp ./build/auto-tools/dist  ./build/auto-docs/docs/.vuepress/dist/tools
+cd ./build/auto-docs/docs/.vuepress/dist # 进入DOC
 # deploy to github
 echo 'auto.ankio.net' > CNAME
 #mv docs/.vuepress/dist ../docs
